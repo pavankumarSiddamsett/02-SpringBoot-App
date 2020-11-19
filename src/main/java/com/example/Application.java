56cddea8cc738736381036b39e7d389c0326a129
pavan <pavanmca8@gmail.com>
2020-11-19 12:50:27 +0530

    HIS-134  Fix privded

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-	
+	//HIS-134 modification
 			context.close();
 	}
 
